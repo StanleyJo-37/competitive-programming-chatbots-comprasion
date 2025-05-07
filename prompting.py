757d94f8f66caba98ac3558da348db09b6d70711
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-05-07 18:21:38 +0700

    # feat add arguments for running script

--- prompting.py
+++ prompting.py
@@ -1,9 +1,12 @@
 import os
 from openai import OpenAI
+from openai import _exceptions as OpenAIExceptions
 from dotenv import load_dotenv
 from google import genai
+from google.genai.errors import APIError, ClientError, UnknownFunctionCallArgumentError
 import pandas as pd
 from typing import Literal
+import sys
 
 Model = Literal['gemini', 'deepseek', 'chatgpt']
 
@@ -13,81 +16,129 @@ deepseek = None
 gemini = None
 chatgpt = None
 
-def initDeepSeekV3Model():
-  api_key = os.getenv("DEEPSEEK_API_KEY")
-  base_url = os.getenv("DEEPSEEK_BASE_URL")
+all_prompts = pd.read_csv('./prompts/input.csv')
 
-  if not api_key:
-    raise Exception("DEEPSEEK_API_KEY not found.")
+def initDeepSeekV3Model() -> bool:
+  try:
+    api_key = os.getenv("DEEPSEEK_API_KEY")
+    base_url = os.getenv("DEEPSEEK_BASE_URL")
+
+    if not api_key:
+      print("DEEPSEEK_API_KEY not found.")
+      return False
+    
+    if not base_url:
+      print("DEEPSEEK_BASE_URL not found.")
+      return False
+
+    client = OpenAI(api_key=api_key,base_url=base_url)
+
+    global deepseek
+    deepseek = client
+    return True
+  except Exception as e:
+    print(f"Unexpected error: {str(e)}")
+    return False
+    
+
+def initGeminiPro25PreviewModel() -> bool:
+  try:
+    api_key = os.getenv("GEMINI_API_KEY")
+
+    if not api_key:
+      print("GEMINI_API_KEY not found.")
+      return False
+
+    client = genai.Client(api_key=api_key)
+
+    global gemini
+    gemini = client
+
+    print("Gemini Initialized.")
+    return True
+  except UnknownFunctionCallArgumentError as e:
+    print(f"Invalid input: {e.message}")
+    return False
+  except APIError as e:
+    print(f"API error: {e.message}")
+    return False
+  except ClientError as e:
+    print(f"API error: {e.message}")
+    return False
+
+def initChatGPT4oModel() -> bool:
+  try:
+    api_key = os.getenv("CHATGPT_API_KEY")
+    base_url = os.getenv("CHATGPT_BASE_URL")
+
+    if not api_key:
+      print("CHATGPT_API_KEY not found.")
+      return True
+    
+    if not base_url:
+      print("CHATGPT_BASE_URL not found.")
+      return True
+
+    client = OpenAI(api_key=api_key, base_url=base_url)
+
+    global chatgpt
+    chatgpt = client
+    return True
+  except Exception as e:
+    print(f"Unexpected error: {str(e)}")
+    return False
   
-  if not base_url:
-    raise Exception("DEEPSEEK_BASE_URL not found.")
+def getPrompt(idx: int) -> str:
+  return all_prompts[idx, 'prompts']
 
-  client = OpenAI(api_key=api_key,base_url=base_url)
+def completePrompt(idx: int) -> None:
+  all_prompts[idx, 'done'] = True
 
-  global deepseek
-  deepseek = client
+def unload() -> None:
+  all_prompts.to_csv('./prompts/input.csv')
 
-def initGeminiPro25PreviewModel():
-  api_key = os.getenv("GEMINI_API_KEY")
-
-  if not api_key:
-    raise Exception("GEMINI_API_KEY not found.")
-
-  client = genai.Client(api_key=api_key)
-
-  global gemini
-  gemini = client
-
-  print("Gemini Initialized.")
-
-def initChatGPT4oModel():
-  api_key = os.getenv("CHATGPT_API_KEY")
-  base_url = os.getenv("CHATGPT_BASE_URL")
-
-  if not api_key:
-    raise Exception("CHATGPT_API_KEY not found.")
-  
-  if not base_url:
-    raise Exception("CHATGPT_BASE_URL not found.")
-
-  client = OpenAI(api_key=api_key, base_url=base_url)
-
-  global chatgpt
-  chatgpt = client
-
-def promptDeepSeek(prompt: str) -> bool:
+def promptDeepSeek(idx: int):
   global deepseek
 
   if deepseek == None:
     raise Exception("DeepSeek model is not initialized.")
   
-  response = deepseek.completions.create(
-    model='deepseek-chat',
-    messages=[
-      {
-        "role": "user",
-        "content": prompt
-      },
-    ],
-    stream=False,
-  )
+  try:
+    response = deepseek.completions.create(
+      model='deepseek-chat',
+      messages=[
+        {
+          "role": "user",
+          "content": getPrompt(idx)
+        },
+      ],
+      stream=False,
+      temperature=0.0,
+    )
+  except OpenAIExceptions.RateLimitError as e:
+    print(f'Rate Limit Error: {e.message}')
+  except Exception as e:
+    print(f'Unexpected Erorr: {str(e)}')
 
   return response.choices[0].message.content
 
-def promptGemini(prompt: str) -> bool:
+def promptGemini(idx: int):
   global gemini
 
   if gemini == None:
     raise Exception("Gemini model is not initialized.")
   
   response = gemini.models.generate_content(
-    model="gemini-2.5-pro-exp-03-25", contents=prompt
+    model="gemini-2.5-pro-exp-03-25",
+    contents=getPrompt(idx),
+    config={
+      'temperature': 0.0,
+    },
   )
 
   return response.text
 
-def promptChatGPT(prompt: str) -> bool:
+def promptChatGPT(idx: int) -> bool:
   global chatgpt
 
   if chatgpt == None:
@@ -98,19 +149,42 @@ def promptChatGPT(prompt: str) -> bool:
     messages=[
       {
         "role": "user",
-        "content": prompt
+        "content": getPrompt(idx)
       },
     ],
     stream=False,
+    temperature=0.0,
   )
 
   return response.choices[0].message.content
 
-def fetchPrompts() -> pd.DataFrame:
-  prompts = pd.read_csv('./prompts/input.csv')
-  prompts = prompts.loc[~prompts['done']]
-
-  return prompts
 
 if __name__ == '__main__':
+  args = sys.argv
+
+  try:
+    model = args[1]
+    number_of_row = int(args[2])
+  except Exception as e:
+    print(f'The second argument must be an integer: {e}')
   
+  if model != 'deepseek' or model != 'chatgpt' or model != 'gemini':
+    raise Exception("The first argument must be deepseek, chatgpt, or gemini")
+
+  indices = all_prompts[~all_prompts['done']].sample(number_of_row).index.sort_values()
+  
+  if model == 'deepseek':
+    initDeepSeekV3Model()
+    fn_call = promptDeepSeek
+  elif model == 'chatgpt':
+    initChatGPT4oModel()
+    fn_call = promptChatGPT
+  elif model == 'gemini':
+    initGeminiPro25PreviewModel()
+    fn_call = promptGemini
+
+  for index in indices:
+    fn_call(index)
+    completePrompt(index)
+
+  unload()
